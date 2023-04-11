3b2b64bd162798026a42f494605a7c01715651af
Brian <bstanton773@gmail.com>
2023-04-11 12:09:49 -0500

    Connect mongoDB to the application

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const express = require('express');
 const app = express();
-const port = 3000;
-const path = require('path')
+const port = process.env.PORT;
+const path = require('path');
+const { connectDB } = require('./src/db');
+
+
+// Execute the connectDB function to connect to our database
+connectDB();
 
 app.set('view engine', 'ejs');
 // Update the location of the folder for res.render to use (default is './views')
