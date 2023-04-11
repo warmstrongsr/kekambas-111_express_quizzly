1501fd99418576457d5cba83377953de995d3ef0
Brian <bstanton773@gmail.com>
2023-04-11 14:35:22 -0500

    Add new users to database on register

--- src/routes/auth/register.js
+++ src/routes/auth/register.js
@@ -1,4 +1,24 @@
-module.exports = (req, res) => {
+const { User } = require('../../models');
+const bcrypt = require('bcrypt');
+
+module.exports = async (req, res) => {
     console.log(req.body);
-    res.send('Post Auth Router Register');
+    // First check if that passwords match
+    if (req.body.password !== req.body.confirmPass){
+        res.status(400).send({ error: "Your passwords do not match" })
+    } else {
+        // Get the data from the request body
+        const { username, email, password } = req.body
+        // Hash and salt the password before storing 
+        const passwordHash = await bcrypt.hash(password, 10);
+        // Create a new instance of User 
+        const user = new User({ username, email, password: passwordHash })
+        try{
+            await user.save();
+            res.send(`New User Created - ${user.username}`);
+        } catch (err){
+            console.log(err);
+            res.send('There was an issue')
+        }
+    }
 }
