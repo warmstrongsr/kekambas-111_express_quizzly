2a7df1abcd11de63c028b93c4248947ff08de174
Brian <bstanton773@gmail.com>
2023-04-11 11:27:46 -0500

    Refactor Auth Router to have separate files for each request

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded( { extended: true }))
 
 // Add logging middleware
 app.use((req, res, next) => {
-    console.log(req.path);
+    console.log(req.method, req.path);
     next();
 })
 
